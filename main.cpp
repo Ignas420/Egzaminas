b493d8fd8274b7d9dd95934c4a654636cc4914c4
Ignas Sarapinas <ignas.sarapinas@mif.stud.vu.lt>
2024-06-07 09:44:00 +0300

    pakeitimai su URL

--- main.cpp
+++ main.cpp
@@ -5,67 +5,80 @@
 #include <string>
 #include <sstream>
 #include <map>
-#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
-string salintiSkyryba(const string& word);
-vector<string> Atrinkimas(const string& eil);
+string salintiSkyryba(const string &zodis);
+vector<string> Atrinkimas(const string &eil);
+bool isURL(const string &zodis);
 
-int main(){
+int main()
+{
     string eilute;
     vector<string> eilutes;
-    unordered_map<string, int> zodzio_kiekis;
-    unordered_map<string, vector<int>> zodzio_pozicija;
+    map<string, int> zodzio_kiekis;
+    map<string, vector<int>> zodzio_pozicija;
     ifstream fd("text.txt");
 
-// Failo skaitymas
-    if(!fd.is_open()){
+    // Failo skaitymas
+    if (!fd.is_open())
+    {
         throw runtime_error("Skaitymo failas neatidaromas!");
     }
 
-    int eil_nr=0;
+    int eil_nr = 0;
 
-    while(getline(fd, eilute)){
+    while (getline(fd, eilute))
+    {
         eilutes.push_back(eilute);
         istringstream iss(eilute);
         string zodis;
-        while(iss >> zodis){
-            string newEil = salintiSkyryba(zodis);
-            if(!newEil.empty()){
-                zodzio_kiekis[newEil]++;
-                zodzio_pozicija[newEil].push_back(eil_nr);
+        while (iss >> zodis)
+        {
+            string newZod = salintiSkyryba(zodis);
+            if (!newZod.empty())
+            {
+                zodzio_kiekis[newZod]++;
+                zodzio_pozicija[newZod].push_back(eil_nr);
             }
         }
         eil_nr++;
     }
     fd.close();
 
-// Pasikartojusiu zodziu isvedimas
+    // Pasikartojusiu zodziu isvedimas
     ofstream fr("pasikartojimai.txt");
-    if(!fr.is_open()){
+    if (!fr.is_open())
+    {
         throw runtime_error("Isvedimo failas neatidaromas!");
     }
 
     fr << "Zodziai, kurie pasikartoja daugiau nei viena karta: " << endl;
-    for(auto i : zodzio_kiekis){
-        if(i.second > 1){
+    for (auto i : zodzio_kiekis)
+    {
+        if (i.second > 1)
+        {
             fr << i.first << ": " << i.second << endl;
         }
     }
     fr.close();
 
-// Cross-reference lenteles isvedimas
+    // Cross-reference lenteles isvedimas
     ofstream fr1("lentele.txt");
-    if(!fr1.is_open()){
+    if (!fr1.is_open())
+    {
         throw runtime_error("Isvedimo failas neatidaromas!");
     }
 
     fr1 << "Cross-reference lentele: " << endl;
-    for(auto i : zodzio_pozicija){
-        if(i.second.size() > 1){
+    for (auto i : zodzio_pozicija)
+    {
+        if (i.second.size() > 1)
+        {
             fr1 << i.first << ": ";
-            for(int pozicija : i.second){
+            for (int pozicija : i.second)
+            {
                 fr1 << pozicija << " ";
             }
             fr1 << endl;
@@ -73,16 +86,19 @@ int main(){
     }
     fr1.close();
 
-// URL atrinkimas
+    // URL atrinkimas
     ofstream fr2("URL.txt");
-    if(!fr2.is_open()){
+    if (!fr2.is_open())
+    {
         throw runtime_error("Isvedimo failas neatidaromas!");
     }
 
     fr2 << "Atrinkti URL: " << endl;
-    for(string eil : eilutes){
+    for (string eil : eilutes)
+    {
         vector<string> URL = Atrinkimas(eil);
-        for(string url : URL){
+        for (string url : URL)
+        {
             fr2 << url << endl;
         }
     }
@@ -90,33 +106,43 @@ int main(){
 
     return 0;
 }
-string salintiSkyryba(const string& word) {
-    string newEil;
-    for (char c : word) {
-        if (isalpha(c) || isdigit(c)) {
-            newEil += tolower(c);
+string salintiSkyryba(const string &zodis)
+{
+    string newZod;
+    for (char c : zodis)
+    {
+        if (isalpha(c) || isdigit(c))
+        {
+            newZod += tolower(c);
         }
     }
-    return newEil;
+    return newZod;
 }
-vector<string> Atrinkimas(const string& eil){
+vector<string> Atrinkimas(const string &eil)
+{
     vector<string> urls;
     size_t pozicija = 0;
 
-    while(pozicija < eil.size()){
+    while (pozicija < eil.size())
+    {
         size_t pradzia = eil.find("http://", pozicija);
-        if(pradzia == string::npos){
-            pradzia = eil.find("http://", pozicija);
+
+        if (pradzia == string::npos)
+        {
+            pradzia = eil.find("https://", pozicija);
         }
-        if(pradzia == string::npos){
+        if (pradzia == string::npos)
+        {
             pradzia = eil.find("www.", pozicija);
         }
-        if(pradzia == string::npos){
+        if (pradzia == string::npos)
+        {
             break;
         }
-        
+
         size_t pabaiga = eil.find_first_of(" \t\n", pradzia);
-        if(pabaiga == string::npos){
+        if (pabaiga == string::npos)
+        {
             pabaiga = eil.size();
         }
 
@@ -126,5 +152,34 @@ vector<string> Atrinkimas(const string& eil){
         pozicija = pabaiga;
     }
 
+    istringstream iss(eil);
+    string zodis;
+
+    while (iss >> zodis)
+    {
+        if (zodis.find('.') != string::npos &&
+            count(zodis.begin(), zodis.end(), '.') == 1 &&
+            all_of(zodis.begin(), zodis.end(), [](char c){ return isalnum(c) || c == '.'; }) &&
+            !isdigit(zodis.front()) && !isdigit(zodis.back()))
+        {
+            if (isURL(zodis))
+            {
+                urls.push_back(zodis);
+            }
+        }
+    }
+
     return urls;
+}
+bool isURL(const string &zodis)
+{
+    const vector<string> Extensions = {".lt", ".com", ".org", ".net", ".gov", ".edu"};
+    for (const string &extension : Extensions)
+    {
+        if (equal(extension.rbegin(), extension.rend(), zodis.rbegin()))
+        {
+            return true;
+        }
+    }
+    return false;
 }
